61e5c635b5a977096c3f706cea481056fecd5291
Ali Kelkawi <ali.kelkawi@canonical.com>
2024-07-05 13:23:03 +0300

    add tests and ci/cd workflows

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,37 +1,66 @@
-#!/usr/bin/env python3
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
+"""Charm integration tests."""
+
 import logging
-from pathlib import Path
+import socket
+import unittest.mock
 
 import pytest
-import yaml
+import requests
+from helpers import APP_NAME, gen_patch_getaddrinfo, get_unit_url
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = METADATA["name"]
-
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
+@pytest.mark.usefixtures("deploy")
+class TestDeployment:
+    """Integration tests for charm."""
 
-    Assert on the unit status before any relations/configurations take place.
-    """
-    # Build and deploy charm from local source folder
-    charm = await ops_test.build_charm(".")
-    resources = {
-        "some-container-image": METADATA["resources"]["some-container-image"]["upstream-source"]
-    }
+    async def test_basic_client(self, ops_test: OpsTest):
+        """Perform GET request on the host."""
+        url = await get_unit_url(ops_test, APP_NAME, 0, 80)
+        logger.info("curling app address: %s", url)
 
-    # Deploy the charm and wait for active/idle status
-    await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
-    )
+        response = requests.get(f"{url}/ready", timeout=300)
+        assert response.status_code == 200
+
+        response = requests.get(url, timeout=300)
+        assert response.status_code == 403
+
+    async def test_ingress(self, ops_test: OpsTest):
+        """Set external-hostname and test connectivity through ingress."""
+        new_hostname = "oauth2-proxy-web"
+        application = ops_test.model.applications[APP_NAME]
+        await application.set_config({"external-hostname": new_hostname})
+
+        async with ops_test.fast_forward():
+            await ops_test.model.wait_for_idle(
+                apps=[APP_NAME, "nginx-ingress-integrator"],
+                status="active",
+                raise_on_blocked=False,
+                idle_period=30,
+                timeout=1200,
+            )
+
+            with unittest.mock.patch.multiple(socket, getaddrinfo=gen_patch_getaddrinfo(new_hostname, "127.0.0.1")):
+                response = requests.get(f"https://{new_hostname}", timeout=5, verify=False)  # nosec
+                assert response.status_code == 403 and "<title>Sign In</title>" in response.text
+
+    async def test_restart_action(self, ops_test: OpsTest):
+        """Test charm restart action."""
+        action = await ops_test.model.applications[APP_NAME].units[0].run_action("restart")
+        await action.wait()
+
+        async with ops_test.fast_forward():
+            await ops_test.model.wait_for_idle(
+                apps=[APP_NAME],
+                status="active",
+                raise_on_blocked=False,
+                timeout=600,
+            )
+
+            assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
