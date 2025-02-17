be98632230402fe9ab920de2ba950cedecced0cc
natalia <natalianowakowska@mail.com>
2025-02-17 16:12:40 +0100

    tests: refactor integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,48 +1,21 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Charm integration test config."""
+import os
 
-import asyncio
-import logging
-
-import pytest_asyncio
-from helpers import APP_NAME, CHARM_CONFIG, METADATA
+import pytest
+from lightkube import Client, KubeConfig
 from pytest_operator.plugin import OpsTest
 
-logger = logging.getLogger(__name__)
+KUBECONFIG = os.environ.get("TESTING_KUBECONFIG", "~/.kube/config")
 
 
-@pytest_asyncio.fixture(name="deploy", scope="module")
-async def deploy(ops_test: OpsTest):
-    """Test the app is up and running."""
-    charm = await ops_test.build_charm(".")
-    resources = {"oauth2-proxy-image": METADATA["resources"]["oauth2-proxy-image"]["upstream-source"]}
+@pytest.fixture(scope="module")
+def client() -> Client:
+    return Client(config=KubeConfig.from_file(KUBECONFIG))
 
-    asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, config=CHARM_CONFIG),
-        ops_test.model.deploy("nginx-ingress-integrator", channel="edge", revision=103, trust=True),
-    )
 
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(
-            apps=[APP_NAME],
-            status="active",
-            raise_on_blocked=False,
-            timeout=600,
-        )
-        await ops_test.model.wait_for_idle(
-            apps=["nginx-ingress-integrator"],
-            status="waiting",
-            raise_on_blocked=False,
-            timeout=600,
-        )
-
-        await ops_test.model.integrate(APP_NAME, "nginx-ingress-integrator")
-
-        await ops_test.model.wait_for_idle(
-            apps=[APP_NAME, "nginx-ingress-integrator"],
-            status="active",
-            raise_on_blocked=False,
-            timeout=600,
-        )
+@pytest.fixture(scope="module")
+def lightkube_client(ops_test: OpsTest) -> Client:
+    lightkube_client = Client(field_manager="oauth2-proxy-k8s", namespace=ops_test.model.name)
+    return lightkube_client
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -4,62 +4,101 @@
 """Charm integration tests."""
 
 import logging
+from os.path import join
+from pathlib import Path
+from typing import Optional
 
 import pytest
 import requests
-from helpers import APP_NAME, get_unit_url
+import yaml
+from lightkube import ApiError, Client
+from lightkube.resources.core_v1 import Service
 from pytest_operator.plugin import OpsTest
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 logger = logging.getLogger(__name__)
 
+METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
+APP_NAME = METADATA["name"]
+IMAGE_PATH = METADATA["resources"]["oauth2-proxy-image"]["upstream-source"]
+TRAEFIK = "traefik-k8s"
 
+
+async def get_k8s_service_address(ops_test: OpsTest, service_name: str, lightkube_client: Client) -> Optional[str]:
+    """Get the address of a LoadBalancer Kubernetes service using kubectl."""
+    try:
+        result = lightkube_client.get(Service, name=service_name, namespace=ops_test.model.name)
+        ip_address = result.status.loadBalancer.ingress[0].ip
+        return ip_address
+    except ApiError as e:
+        logger.error(f"Error retrieving service address: {e}")
+        return None
+
+
+async def get_reverse_proxy_app_url(
+    ops_test: OpsTest, ingress_app_name: str, app_name: str, lightkube_client: Client
+) -> str:
+    """Get the ingress address of an app."""
+    address = await get_k8s_service_address(ops_test, f"{ingress_app_name}-lb", lightkube_client)
+    proxy_app_url = f"http://{address}/{ops_test.model.name}-{app_name}/"
+    logger.debug(f"Retrieved address: {proxy_app_url}")
+    return proxy_app_url
+
+
+@pytest.mark.skip_if_deployed
 @pytest.mark.abort_on_fail
-@pytest.mark.usefixtures("deploy")
-class TestDeployment:
-    """Integration tests for charm."""
+async def test_build_and_deploy(ops_test: OpsTest) -> None:
+    """Build and deploy oauth2-proxy."""
+    charm = await ops_test.build_charm(".")
 
-    async def test_basic_client(self, ops_test: OpsTest):
-        """Perform GET request on the host."""
-        url = await get_unit_url(ops_test, APP_NAME, 0, 80)
-        logger.info("curling app address: %s", url)
+    await ops_test.model.deploy(
+        application_name=APP_NAME,
+        entity_url=charm,
+        resources={"oauth2-proxy-image": IMAGE_PATH},
+        series="jammy",
+        trust=True,
+    )
 
-        response = requests.get(f"{url}/ready", timeout=300)
-        assert response.status_code == 200
+    await ops_test.model.wait_for_idle(
+        raise_on_blocked=False,
+        raise_on_error=True,
+        status="active",
+        timeout=1000,
+    )
+    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
-        response = requests.get(url, timeout=300)
-        assert response.status_code == 403
 
-    async def test_ingress(self, ops_test: OpsTest):
-        """Set external-hostname and test connectivity through ingress."""
-        new_hostname = "oauth2-proxy-web"
-        application = ops_test.model.applications[APP_NAME]
-        await application.set_config({"external-hostname": new_hostname})
+@pytest.mark.skip_if_deployed
+@pytest.mark.abort_on_fail
+async def test_ingress_relation(ops_test: OpsTest) -> None:
+    await ops_test.model.deploy(
+        TRAEFIK,
+        channel="latest/edge",
+        trust=True,
+    )
 
-        async with ops_test.fast_forward():
-            await ops_test.model.wait_for_idle(
-                apps=[APP_NAME, "nginx-ingress-integrator"],
-                status="active",
-                raise_on_blocked=False,
-                idle_period=30,
-                timeout=1200,
-            )
+    await ops_test.model.integrate(f"{APP_NAME}:ingress", TRAEFIK)
 
-            response = requests.get(
-                "https://127.0.0.1", timeout=5, verify=False, headers={"Host": new_hostname}
-            )  # nosec
-            assert response.status_code == 403 and "<title>Sign In</title>" in response.text
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME, TRAEFIK],
+        status="active",
+        raise_on_blocked=False,
+        timeout=1000,
+    )
 
-    async def test_restart_action(self, ops_test: OpsTest):
-        """Test charm restart action."""
-        action = await ops_test.model.applications[APP_NAME].units[0].run_action("restart")
-        await action.wait()
 
-        async with ops_test.fast_forward():
-            await ops_test.model.wait_for_idle(
-                apps=[APP_NAME],
-                status="active",
-                raise_on_blocked=False,
-                timeout=600,
-            )
+@retry(
+    wait=wait_exponential(multiplier=3, min=1, max=20),
+    stop=stop_after_attempt(10),
+    reraise=True,
+)
+async def test_health_checks(ops_test: OpsTest, lightkube_client: Client) -> None:
+    base_url = await get_reverse_proxy_app_url(ops_test, TRAEFIK, APP_NAME, lightkube_client)
 
-            assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+    health_check_url = join(base_url, "ping")
+
+    resp = requests.get(health_check_url, timeout=300, verify=False)
+    assert resp.status_code == 200
+
+    response = requests.get(base_url, timeout=300, verify=False)
+    assert response.status_code == 403
